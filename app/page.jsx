28c10c244e5ec91e69775ae45b48543df6b1c112
moeinparvizi <giventulip@gmail.com>
2023-12-03 12:10:24 +0330

    start project

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="grid place-content-center bg-black text-white h-screen place-items-center">
+      <div className='w-full h-full text-red-500'>
+        moein parvizi start project
+      </div>
+    </main>
+  )
+}
