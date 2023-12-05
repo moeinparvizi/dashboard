06198b6e2d05b0192251cf818c0de74bc19a58b7
moeinparvizi <giventulip@gmail.com>
2023-12-05 16:20:06 +0330

    add animation to footer

--- app/dashboard/components/footer/footer.jsx
+++ app/dashboard/components/footer/footer.jsx
@@ -1,11 +1,21 @@
+"use client"
+
+import { motion } from "framer-motion"
 import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer className="flex justify-between  mt-7 text-textHard items-center bg-bgDark p-5 rounded-lg">
+    <motion.footer
+      initial={{ y: -100 }}
+      whileInView={{ y: 0 }}
+      className="flex justify-between  mt-7 text-textHard items-center bg-bgDark p-5 rounded-lg"
+    >
       <div className="flex gap-2 items-center">
         create by
-        <Link className="font-semibold text-lg text-accent" href="https://linkedin.com/in/moeinparvizi2002">
+        <Link
+          className="font-semibold text-lg text-accent"
+          href="https://linkedin.com/in/moeinparvizi2002"
+        >
           moein parvizi
         </Link>
       </div>
@@ -14,7 +24,7 @@ const Footer = () => {
           see more project
         </Link>
       </div>
-    </footer>
+    </motion.footer>
   )
 }
 
